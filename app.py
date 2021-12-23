59a6d1474afdc8c36347132b8752360a2185346f
rulidor <rulidor90@gmail.com>
2021-12-23 12:02:15 +0200

    merged tests branch and layers branch. fixed app, added main

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, flash, abort, Markup
+
+import recoSystem
+from main import main_trigger
 from recoSystem import RecoSystem
+
 app = Flask(__name__)
 
 app.secret_key = "manbearpig_MUDMAN888"
@@ -10,35 +14,20 @@ def hello():
     # return "Hello, World!"
     return render_template("index.html")
 
-# import recoSystem
-# from flask import Flask, render_template, request, flash, abort, Markup
-
-# app = Flask(__name__)
-# app.secret_key = "manbearpig_MUDMAN888"
-
-
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-
-
 @app.route("/extract_data", methods=['POST', 'GET'])
 def extract_keywords_from_landing_page():
-    reco = RecoSystem()
     url = str(request.form['url'])
-    # result = reco.extract_keywords_from_landing_page(url)
-    # res_title = reco.extract_title_from_landing_page(url)
-    result = reco.scrap_page(url)
-    list_of_images = []
-    for k in result["keywords"]:
-        result_images = reco.recommend_n_photos_by_keywords("pixable", [k], 1)
-        if result_images:
-            list_of_images.append(result_images[0])
-    # if len(result) == 0:
-    #     return func.HttpResponse("Can't extract the data from this url... working on it:)")
+    result =main_trigger(url)
     if not result:
         flash("Can't extract the data from this url... working on it:)")
         return render_template("index.html")
+    images = result["images"]
+    list_of_images = []
+    for k in result["keywords"]:
+        if images[k]: #todo???
+         list_of_images.append(images[k][0])
+    # if len(result) == 0:
+    #     return func.HttpResponse("Can't extract the data from this url... working on it:)")
     res_txt = "<b>Title: </b><br>"
     res_txt += result["title"]
     res_txt += "<br>"
@@ -55,7 +44,20 @@ def extract_keywords_from_landing_page():
 # check2
 if __name__ == '__main__':
     app.run()
-#     # reco = recoSystem.RecoSystem()
-#     # url = "https://www.sonoviastore.co.il/blogs/news/%D7%94%D7%9E%D7%A1%D7%9B%D7%94-%D7%94%D7%98%D7%95%D7%91%D7%94-%D7%91%D7%99%D7%95%D7%AA%D7%A8-%D7%9C%D7%98%D7%99%D7%A1%D7%95%D7%AA-%D7%91%D7%A9%D7%A0%D7%AA-2021-%D7%A1%D7%95%D7%A0%D7%95%D7%91%D7%99%D7%94?utm_source=taboola&utm_medium=referral&utm_campaign=TB_Blog_AirTravel_IL_PC&tblci=GiDbJRndUImP9rc80Mls7KW1gFpDdEMCGlkTelmGFUrFzyDq3U8oxaqc6ZT4jY_QAQ#tblciGiDbJRndUImP9rc80Mls7KW1gFpDdEMCGlkTelmGFUrFzyDq3U8oxaqc6ZT4jY_QAQ"
-#     # result = reco.extract_keywords_from_landing_page(url)
-#     # print(result)
+    # reco = recoSystem.RecoSystem()
+    # url = "https://finance.yahoo.com/news/meatech-3d-reports-breakthrough-cultured-130000799.htm[…]MeaTech+-+Group+3+-+Wide+-+Mobile+-+8%2F12%2F21&guccounter=1"
+    # result = reco.scrap_page(url)
+    # # result = main_trigger(url)
+    # print(result)
+
+
+# import recoSystem
+# from flask import Flask, render_template, request, flash, abort, Markup
+
+# app = Flask(__name__)
+# app.secret_key = "manbearpig_MUDMAN888"
+
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
---
+++ serviceLayer/responsePackage/response.py
@@ -0,0 +1,38 @@
+is_error=False
+Exception=None
+Messege=""
+Kywords=[]
+Title=""
+
+def sign_error(e, Mes):
+  is_error = True
+  Exception = e
+  return None
+
+def add_messegr(mes):
+  Messege = mes
+  return None
+
+def add_keywords(keywords):
+  Keywords=keywords
+  return None
+
+def add_title(title):
+  Title=title
+  return None
+
+def is_error():
+  return is_error
+
+def get_exeption():
+  return Exception
+
+def get_messege():
+  return Messege
+
+def get_title():
+  return Title
+
+
+
+
